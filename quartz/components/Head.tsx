39f9a28ee44859b4d61e2046466e25847ab9fa74
Kaiyang <kaiyoungyu@outlook.com>
2024-03-01 11:20:59 +0800

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -43,7 +43,7 @@ export default (() => {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-4JWCJR9X3X"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
         
           gtag('config', 'G-4JWCJR9X3X');
